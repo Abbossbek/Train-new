2eca9291474e0707c15d74cf46474dcef83578d2
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-03-23 12:24:45 +0500

    last update

--- Train/MainWindow.xaml.cs
+++ Train/MainWindow.xaml.cs
@@ -29,14 +29,9 @@ namespace Train
             web_browser.Address = Environment.CurrentDirectory + "\\Pages\\о станции.html";
             web_browser.FrameLoadEnd += MyBrowserOnFrameLoadEnd;
 
-            frame.Width = frame.MaxWidth - 20;
-            frame_raschot.Width = frame_raschot.MaxWidth - 20;
+            frame.Width = frame.Width - 20;
+            frame_raschot.Width = frame_raschot.Width - 20;
 
-            DoubleAnimation frameHeight = new DoubleAnimation();
-            frameHeight.From = frame.ActualHeight;
-            frameHeight.To = frame.MaxHeight;
-            frameHeight.Duration = TimeSpan.FromSeconds(0.75);
-            frame.BeginAnimation(DockPanel.HeightProperty, frameHeight);
         }
 
         private void MyBrowserOnFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
@@ -52,36 +47,10 @@ namespace Train
         {
             worker = new TrainWorker();
 
+            cmb_export.Items.Add("Внутригосударственный");
+            cmb_export.Items.Add("На экспорт");
             cmb_send_staions.Items.Add("Назарбек");
-            cmb_owner.Items.Add("Инвентарный");
-            cmb_owner.Items.Add("Собственный/арендованный");
-            foreach (var item in worker.carriageTypes)
-            {
-                cmb_type_cargo.Items.Add(item);
-            }
-
-            List<string> list_stations = new List<string>();
-            
-            for (int i = 0; i < worker.whiteStations.Rows.Count; i++)
-            {
-                list_stations.Add(worker.whiteStations.Rows[i].ItemArray[0].ToString()+". ("+ worker.whiteStations.Rows[i].ItemArray[1].ToString()+" км)");
-            }
-            for (int i = 0; i < worker.yellowStations.Rows.Count; i++)
-            {
-                list_stations.Add(worker.yellowStations.Rows[i].ItemArray[0].ToString() + ". (" + worker.yellowStations.Rows[i].ItemArray[1].ToString() + " км)");
-            }
-            for (int i = 0; i < worker.greenStations.Rows.Count; i++)
-            {
-                list_stations.Add(worker.greenStations.Rows[i].ItemArray[0].ToString() + ". (" + worker.greenStations.Rows[i].ItemArray[1].ToString() + " км)");
-            }
-
-            list_stations.Sort();
-
-            foreach(var station in list_stations)
-            {
-                cmb_get_staions.Items.Add(station);
-            }
-
+            cmb_export.SelectedIndex = 0;
             cmb_send_staions.SelectedIndex = 0;
 
             password = worker.mainTable.Rows[4].ItemArray[5].ToString();
@@ -96,47 +65,49 @@ namespace Train
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            web_browser.Visibility = Visibility.Visible;
-            if (sender.Equals(btn_main1))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\о станции.html");
-                Dispatcher.Invoke(() =>
-                {
-                    web_browser.SetZoomLevel((Convert.ToDouble(web_browser.Tag) - 30) / 25.0);
-                });
-            }
-            if (sender.Equals(btn_main2))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\П-пути.html");
-            }
-            if (sender.Equals(btn_main4))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\Документы.html");
-            }
-            if (sender.Equals(btn_main5))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\sxema.html");
-            }
-            if (sender.Equals(btn_main6))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\Типы жд вагонов - Cargo Star LLC.html");
-            }
-            if (sender.Equals(btn_main7))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\Перечень транспортно-экспедиторских организаций.html");
-            }
-            if (sender.Equals(btn_main8))
-            {
-                web_browser.Load(Environment.CurrentDirectory + "\\Pages\\АО «Узбекистон темир йуллари» — Льготы грузоотправителям.html");
-            }
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation frameUp = new DoubleAnimation(1200, 15, new Duration(new TimeSpan(0, 0, 1)));
+            Storyboard.SetTargetProperty(frameUp, new PropertyPath("(Canvas.Top)"));
+            storyboard.Children.Add(frameUp);
 
+            web_browser.Visibility = Visibility.Hidden;
             if (!sender.Equals(btn_main3))
-            {            
-                DoubleAnimation frameHeight = new DoubleAnimation();
-                frameHeight.From = frame.ActualHeight;
-                frameHeight.To = frame.MaxHeight;
-                frameHeight.Duration = TimeSpan.FromSeconds(0.75);
-                frame.BeginAnimation(DockPanel.HeightProperty, frameHeight);
+            {
+
+                web_browser.Visibility = Visibility.Visible;
+                if (sender.Equals(btn_main1))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\о станции.html");
+                    Dispatcher.Invoke(() =>
+                    {
+                        web_browser.SetZoomLevel((Convert.ToDouble(web_browser.Tag) - 30) / 25.0);
+                    });
+                }
+                if (sender.Equals(btn_main2))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\П-пути.html");
+                }
+                if (sender.Equals(btn_main4))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\Документы.html");
+                }
+                if (sender.Equals(btn_main5))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\sxema.html");
+                }
+                if (sender.Equals(btn_main6))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\Типы жд вагонов - Cargo Star LLC.html");
+                }
+                if (sender.Equals(btn_main7))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\Перечень транспортно-экспедиторских организаций.html");
+                }
+                if (sender.Equals(btn_main8))
+                {
+                    web_browser.Load(Environment.CurrentDirectory + "\\Pages\\АО «Узбекистон темир йуллари» — Льготы грузоотправителям.html");
+                }
+                frame.BeginStoryboard(storyboard);
             }
             else
             {
@@ -145,23 +116,20 @@ namespace Train
                 cmb_type_cargo.SelectedIndex = -1;
                 txt_weight.Text = "";
 
-                txtblock_last_price.Text = "Итоговая стоимость:  0 сум";
-                
-                DoubleAnimation frameHeight = new DoubleAnimation();
-                frameHeight.From = frame_raschot.ActualHeight;
-                frameHeight.To = frame_raschot.MaxHeight;
-                frameHeight.Duration = TimeSpan.FromSeconds(0.75);
-                frame_raschot.BeginAnimation(DockPanel.HeightProperty, frameHeight);
+                txtblock_last_price.Text = "Стоимость тарифа:  0 сум";
+
+                frame_raschot.BeginStoryboard(storyboard);
             }
+
         }
 
         private void btn_colculation_Click(object sender, RoutedEventArgs e)
         {
-            if(cmb_send_staions.SelectedItem==null ||
-                cmb_get_staions.SelectedItem==null ||
+            if (cmb_send_staions.SelectedItem == null ||
+                cmb_get_staions.SelectedItem == null ||
                 cmb_type_cargo.SelectedItem == null ||
                 cmb_owner.SelectedItem == null ||
-                txt_weight.Text=="" ||
+                txt_weight.Text == "" ||
                 !Int32.TryParse(txt_weight.Text, out int t))
             {
                 MessageBox.Show("Пожалуйста, заполните необходимые разделы и проверьте правильность!");
@@ -174,47 +142,106 @@ namespace Train
                     MessageBox.Show("Минимальная масса груза 10 тонн!");
                     return;
                 }
-                else if(weight>80)
+                else if (weight > 80)
                 {
                     MessageBox.Show("Максимальная масса груза 80 тонн!");
                     return;
                 }
 
                 string selected_station = cmb_get_staions.SelectedItem.ToString().Remove(cmb_get_staions.SelectedItem.ToString().IndexOf('.'));
+                if(cmb_get_staions.SelectedItem.ToString().Contains("через"))
+                    selected_station = selected_station.Remove(0,cmb_get_staions.SelectedItem.ToString().IndexOf("через ")+6);
+
                 string selected_type_cargo = cmb_type_cargo.SelectedItem.ToString();
                 bool inventory = cmb_owner.SelectedIndex == 0;
 
                 double last_price = worker.Calculate(selected_station, selected_type_cargo, inventory, weight);
 
-                txtblock_last_price.Text = "Итоговая стоимость:  " + String.Format("{0:#,###}",Math.Round(last_price)) + " сум";
+                txtblock_last_price.Text = "Стоимость тарифа:  " + String.Format("{0:#,###}", Math.Round(last_price)) + " сум";
             }
         }
 
 
         private void web_browser_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
         {
-                    if (e.Delta > 0 && web_browser.ZoomLevel <= 1000)
-                    {
+            if (e.Delta > 0 && web_browser.ZoomLevel <= 1000)
+            {
                 web_browser.ZoomInCommand.Execute(null);
-                    }
-                    else if (e.Delta < 0 && web_browser.ZoomLevel >= 10)
-                    {
+            }
+            else if (e.Delta < 0 && web_browser.ZoomLevel >= 10)
+            {
                 web_browser.ZoomOutCommand.Execute(null);
-                    }
+            }
         }
 
-
-
-        private void btn_keyboard_Click(object sender, RoutedEventArgs e)
+        private void cmb_export_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (row_keyboard.Height.Value != 0)
+            if (cmb_export.SelectedIndex == 0)
             {
-                row_keyboard.Height = new GridLength(0);
+                List<string> list_stations = new List<string>();
+
+                for (int i = 0; i < worker.whiteStations.Rows.Count; i++)
+                {
+                    list_stations.Add(worker.whiteStations.Rows[i].ItemArray[0].ToString() + ". (" + worker.whiteStations.Rows[i].ItemArray[1].ToString() + " км)");
+                }
+                for (int i = 0; i < worker.yellowStations.Rows.Count; i++)
+                {
+                    list_stations.Add(worker.yellowStations.Rows[i].ItemArray[0].ToString() + ". (" + worker.yellowStations.Rows[i].ItemArray[1].ToString() + " км)");
+                }
+                for (int i = 0; i < worker.greenStations.Rows.Count; i++)
+                {
+                    list_stations.Add(worker.greenStations.Rows[i].ItemArray[0].ToString() + ". (" + worker.greenStations.Rows[i].ItemArray[1].ToString() + " км)");
+                }
+
+                list_stations.Sort();
+                cmb_get_staions.ItemsSource = list_stations;
+                cmb_type_cargo.Items.Clear();
+                foreach (var item in worker.carriageTypes)
+                {
+                    cmb_type_cargo.Items.Add(item);
+                }
             }
             else
             {
-                row_keyboard.Height = new GridLength(200);
+                List<string> list_stations = new List<string>();
+
+                for (int i = 0; i < worker.exportStations.Rows.Count; i++)
+                {
+                    list_stations.Add(worker.exportStations.Rows[i].ItemArray[2].ToString() + " через " + worker.exportStations.Rows[i].ItemArray[0].ToString() + ". (" + worker.exportStations.Rows[i].ItemArray[1].ToString() + " км)");
+                }
+
+                cmb_get_staions.ItemsSource = list_stations;
+                cmb_type_cargo.Items.Clear();
+                for (int i = 0; i<3; i++)
+                {
+                    cmb_type_cargo.Items.Add(worker.carriageTypes[i]);
+                }
             }
+        }
+
+        private void cmb_get_staions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmb_type_cargo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cmb_owner.Items.Clear();
+                cmb_owner.Items.Add("Инвентарный");
+            if (!cmb_type_cargo.SelectedItem?.ToString().Equals("Вагон-термос")??true)
+            {
+            cmb_owner.Items.Add("Собственный/арендованный");
+            }
+        }
+
+        private void txt_weight_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txt_weight_LostFocus(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void key_Click(object sender, RoutedEventArgs e)
@@ -229,9 +256,9 @@ namespace Train
             if (sender == key_7) txt_weight.Text += "7";
             if (sender == key_8) txt_weight.Text += "8";
             if (sender == key_9) txt_weight.Text += "9";
-            if (sender == key_delete && txt_weight.Text.Length>0) txt_weight.Text = txt_weight.Text.Remove(txt_weight.Text.Length - 1);
-            if (sender == key_clear) txt_weight.Text=""; 
-            
+            if (sender == key_delete && txt_weight.Text.Length > 0) txt_weight.Text = txt_weight.Text.Remove(txt_weight.Text.Length - 1);
+            if (sender == key_clear) txt_weight.Text = "";
+
         }
         private void Image_TouchDown(object sender, System.Windows.Input.TouchEventArgs e)
         {
@@ -244,20 +271,14 @@ namespace Train
         }
         private void btn_exit_frame1_Click(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation frameHeight = new DoubleAnimation();
-            frameHeight.From = frame.ActualHeight;
-            frameHeight.To = 0;
-            frameHeight.Duration = TimeSpan.FromSeconds(0.75);
-            frame.BeginAnimation(DockPanel.HeightProperty, frameHeight);
-
-            DoubleAnimation frameHeight_raschot = new DoubleAnimation();
-            frameHeight_raschot.From = frame_raschot.ActualHeight;
-            frameHeight_raschot.To = 0;
-            frameHeight_raschot.Duration = TimeSpan.FromSeconds(0.75);
-            frame_raschot.BeginAnimation(DockPanel.HeightProperty, frameHeight_raschot);
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation frameUp = new DoubleAnimation(15, 1200, new Duration(new TimeSpan(0, 0, 1)));
+            Storyboard.SetTargetProperty(frameUp, new PropertyPath("(Canvas.Top)"));
+            storyboard.Children.Add(frameUp);
+            if (Canvas.GetTop(frame_raschot) < 1000)
+                frame_raschot.BeginStoryboard(storyboard);
+            else
+                frame.BeginStoryboard(storyboard);
         }
-
-
     }
-    
 }
--- Train/Model/TrainWorker.cs
+++ Train/Model/TrainWorker.cs
@@ -16,6 +16,7 @@ namespace Train.Model
         public DataTable whiteStations,
             yellowStations,
             greenStations,
+            exportStations,
             mainTable;
         DataSet mainDataSet;
         DataTable[] prices = new DataTable[28];
@@ -36,6 +37,8 @@ namespace Train.Model
                 yellowStations = dataSet.Tables[1];
                 greenStations = new DataTable();
                 greenStations = dataSet.Tables[2];
+                exportStations = new DataTable();
+                exportStations = dataSet.Tables[3];
             }
             using (var stream = File.Open(Environment.CurrentDirectory + "\\Files\\Стоимость.xlsx", FileMode.Open, FileAccess.Read))
             {
@@ -65,6 +68,7 @@ namespace Train.Model
         {
             int way_length = 0;
             double coefficient = 1;
+            int indexX = 0, indexY=0;
 
             for (int i = 0; i < whiteStations.Rows.Count; i++)
             {
@@ -93,62 +97,134 @@ namespace Train.Model
                     break;
                 }
             }
+            for (int i = 0; i < exportStations.Rows.Count; i++)
+            {
+                if (exportStations.Rows[i].ItemArray[0].ToString().Equals(selected_station))
+                {
+                    switch (exportStations.Rows[i].ItemArray[3].ToString())
+                    {
+                        case "1":
+                            coefficient = Convert.ToDouble(coefficient1);
+                            break;
+                        case "2":
+                            coefficient = Convert.ToDouble(coefficient2);
+                            break;
+                        default:
+                            coefficient = Convert.ToDouble(coefficient3);
+                            break;
+                    }
+                    way_length = Int32.Parse(exportStations.Rows[i].ItemArray[1].ToString());
+                    break;
+                }
+            }
+
+            int tableIndex = 0;
+            long sum = 0;
 
             switch (selected_type_cargo)
             {
                 case "Крытый вагон":
                 case "Полувагон":
                 case "Платформа":
-                    
+                    tableIndex = inventory?1:2;
+                    indexX = FindX(way_length);
+                    indexY = weight - 9;
+                    sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
                 case "Зерновоз":
+                    tableIndex = 3;
+                    indexX = FindX(way_length,2);
+                    if (weight < 70)
+                    {
+                        indexY = inventory ? 8 : 24;
+                        sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
+                    }
+                    else
+                    {
+                        indexY = inventory ? 9 : 25;
+                        sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY-1].ItemArray[indexX].ToString().Replace(" ", "")) +
+                            (weight - 70) * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
+                    }
                     break;
                 case "Хоппер-цементовоз":
+                    tableIndex = 3;
+                    indexX = FindX(way_length,2);
+                    if (weight < 70)
+                    {
+                        indexY = inventory ? 8 : 19;
+                        sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
+                    }
+                    else
+                    {
+                        indexY = inventory ? 9 : 20;
+                        sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY - 1].ItemArray[indexX].ToString().Replace(" ", "")) +
+                            (weight - 70) * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
+                    }
                     break;
                 case "Цистерна-цементовоз":
                 case "Хоппер-дозатор":
                 case "Минераловоз":
-                case "Думпкары":
-                case "Бункерные полувагоны для битума":
+                case "Думпкар":
+                case "Бункерный полувагон для битума":
+                    tableIndex = 3;
+                    indexX = FindX(way_length,2);
+                    if (weight < 70)
+                    {
+                        indexY = inventory ? 11 : 24;
+                        sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
+                    }
+                    else
+                    {
+                        indexY = inventory ? 12 : 25;
+                        sum = Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY - 1].ItemArray[indexX].ToString().Replace(" ", "")) +
+                            (weight - 70) * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
+                    }
                     break;
                 case "Вагон-термос":
+                    tableIndex = 3;
+                    indexX = FindX(way_length,2);
+                    indexY = 38;
+                    sum = weight < 70 
+                        ? Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""))
+                        : Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", "")) +
+                            (weight - 70) * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY+1].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
                 case "Цистерна (Нефтепродукты)":
+                    tableIndex = 6;
+                    indexX = FindX(way_length);
+                    indexY = inventory ? 4 : 5;
+                    sum = weight * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
                 case "Цистерна (Газы сжиженные)":
+                    tableIndex = 6;
+                    indexX = FindX(way_length);
+                    indexY = inventory ? 6 : 7;
+                    sum = weight * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
                 case "Цистерна (Спирты и фенолы)":
+                    tableIndex = 6;
+                    indexX = FindX(way_length);
+                    indexY = inventory ? 8 : 9;
+                    sum = weight * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
                 case "Цистерна (Скоропортящиеся грузы)":
+                    tableIndex = 6;
+                    indexX = FindX(way_length);
+                    indexY = inventory ? 10 : 11;
+                    sum = weight * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
                 case "Цистерна (Остальные наливные грузы)":
+                    tableIndex = 6;
+                    indexX = FindX(way_length);
+                    indexY = inventory ? 12 : 13;
+                    sum = weight * Convert.ToInt64(mainDataSet.Tables[tableIndex].Rows[indexY].ItemArray[indexX].ToString().Replace(" ", ""));
                     break;
             }
-            int index_i = find_i(weight);
-            int index_j = find_j(way_length);
 
-            long price = 0;
-
-            string s = mainTable.Rows[index_i].ItemArray[index_j].ToString().Replace(" ", "");
-
-            if (s.Contains(" "))
-            {
-                s.Replace(" ", "");
-            }
-
-            if (index_i == 73)
-            {
-                price = weight * Int32.Parse(s);
-            }
-            else
-            {
-                price = Int64.Parse(s);
-            }
-
-           return price * coefficient * (Convert.ToDouble(nds) / 100 + 1);
+           return sum * coefficient * (Convert.ToDouble(nds) / 100 + 1);
         }
 
-        private int find_j(int way_length, int type = 1)
+        private int FindX(int way_length, int type = 1)
         {
             int distance = type == 1 ? 11 : 2;
             if (way_length <= 50)
@@ -195,18 +271,6 @@ namespace Train.Model
                 }
             }
 
-        }
-
-        private int find_i(int weight)
-        {
-            if (weight <= 80)
-            {
-                return (weight - 9);
-            }
-            else
-            {
-                return 73; // При весе свыше 80 т	
-            }
         }
     }
 }
